9b3d31fb86201f690ed166319071081a88163721
GianCampos20 <gianfrancocamposfp@gmail.com>
2025-02-14 14:07:07 +0100

    Nuevos cambios de mi portfolio

--- dist/portfolio-personal/server/assets-chunks/index_html.mjs
+++ dist/portfolio-personal/server/assets-chunks/index_html.mjs
@@ -6,7 +6,7 @@ export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body><!--nghm-->
-  <app-root ng-version="19.1.6" ngh="0" ng-server-context="ssg"><h1>ESTO ES SOLO EL COMIENZO DE MI PORTFOLIO</h1></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-USZLIDT6.js" type="module"></script>
+  <app-root ng-version="19.1.6" ngh="0" ng-server-context="ssg"><h1>ESTO AHORA ES UNA PRUEBA DE COMO ACTUALIZAR</h1><p>Este es mi parrafo para hacer todo</p></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-E3FCFRL3.js" type="module"></script>
 
 <script id="ng-state" type="application/json">{"__nghData__":[{}]}</script></body></html>`;
